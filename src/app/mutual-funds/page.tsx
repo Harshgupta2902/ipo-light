c7211bef7cc0a63d7e7be2564b373044db12748c
Harsh <harsh.gupta@cognustechnology.com>
2024-06-11 16:01:13 +0530

    mf-homepage

--- src/app/mutual-funds/page.tsx
+++ src/app/mutual-funds/page.tsx
@@ -1,9 +1,68 @@
 import ImageFallback from "@/components/common/ImageFallback";
-import Link from "next/link";
 import React from "react";
 import SimpleSlider from "@/components/home/Slider";
+import Accordion from "@/components/common/Accordion";
 
 export default function MutualFundsHomePage() {
+    const categories = [
+        {
+            title: "Equity Mutual Funds",
+            description: "High-growth potential with long-term capital gains.",
+            imageSrc: "/equity.png",
+            link: "/mutual-funds/equity",
+            imageAlt: "Equity Mutual Funds"
+        },
+        {
+            title: "Debt Mutual Funds",
+            description: "Stable returns with lower risk.",
+            imageSrc: "/debt.png",
+            link: "/mutual-funds/debt",
+            imageAlt: "Debt Mutual Funds"
+        },
+        {
+            title: "Hybrid Mutual Funds",
+            description: "Balanced risk and reward with mixed investments.",
+            imageSrc: "/hybrid.png",
+            link: "/mutual-funds/hybrid",
+            imageAlt: "Hybrid Mutual Funds"
+        }
+    ];
+
+    const mfFaq = [
+        {
+          question: "Is investing in mutual funds profitable?",
+          answer: "Mutual funds are a type of investment where investors pool their money to invest in a diversified portfolio of stocks, bonds, and other securities. They can be profitable if you choose the right fund and stay invested for the long term."
+        },
+        {
+          question: "How to choose a mutual fund that suits your financial objectives?",
+          answer: "To choose a mutual fund, first decide on your risk tolerance and investment horizon. Once you determine these, you can select the best mutual fund that aligns with your financial goals. Consider factors such as potential returns, risk levels, and fund categories like high return, tax-saving, and top-performing funds."
+        },
+        {
+          question: "Can I withdraw from a mutual fund anytime?",
+          answer: "Yes, you can withdraw your investment anytime if you have invested in open-ended mutual funds. However, certain types like ELSS (Equity Linked Savings Schemes) have lock-in periods."
+        },
+        {
+          question: "How to invest in mutual funds?",
+          answer: "You can invest in mutual funds through various platforms. It's typically done online via websites or mobile apps offered by investment platforms."
+        },
+        {
+          question: "How long does it take to start investing in mutual funds?",
+          answer: "The account opening process for investing in mutual funds is straightforward and often paperless. With all required details on hand, it can take as little as a few minutes to get started."
+        },
+        {
+          question: "What are the charges or commissions on mutual fund investments?",
+          answer: "Many platforms offer commission-free mutual fund investments. It's important to choose a platform that offers competitive or zero commission options to maximize your returns."
+        },
+        {
+          question: "Are mutual funds taxable? What are the tax implications on withdrawals?",
+          answer: "Yes, mutual funds are taxable based on the type and duration of investment. For instance, equity-oriented funds held for less than 12 months are subject to short-term capital gains tax. Long-term gains exceeding ₹1 lakh are taxed differently. Debt-oriented funds have different tax implications based on the holding period and the fund's exposure to stocks."
+        },
+        {
+          question: "How do investors earn returns from mutual funds?",
+          answer: "Returns from mutual funds come in two forms: dividends and capital gains. Dividends are distributions made by funds from their profits, while capital gains arise from selling investments at a higher price than purchased. Both dividends and capital gains are taxable."
+        }
+    ];    
+
     return (
         <main>
             <section className="lg:pt-20 sm:pt-0">
@@ -26,14 +85,13 @@ export default function MutualFundsHomePage() {
                                 className="mx-auto"
                                 width="800"
                                 height="420"
-                                alt="banner image"
+                                alt="Mutual Funds"
                                 priority
                             />
                         </div>
                     </div>
                 </div>
             </section>
-
             <div className="section">
                 <div className="container">
                     <div className="row items-center justify-between">
@@ -41,24 +99,64 @@ export default function MutualFundsHomePage() {
                     </div>
                 </div>
             </div>
-
             <section className="lg:pt-20 sm:pt-0">
                 <div className="container">
                     <div className="flex justify-between items-center max-sm:items-start">
                         <div className="text-[48px] font-clashSemibold text-[#0A0F1E] max-md:text-2xl">
                             Explore Mutual Funds
                         </div>
-
                         <a
                             className="flex items-center py-4 px-8 rounded-[10px] btn btn-primary text-white font-inter text-base font-medium hover:bg-primary cursor-pointer max-sm:px-4 max-sm:py-2 max-sm:text-xs max-sm:rounded"
-                            href={"/mutual-funds/screener"}
-                        >
+                            href={"/mutual-funds/screener"} >
                             <span className="shrink-0">Explore More</span>
                         </a>
                     </div>
-
                 </div>
             </section>
+            <section className="section">
+                <div className="container">
+                    <div className="row gy-4">
+                        {categories.map((category, index) => (
+                            <div key={index} className="md:col-6 lg:col-4 xl:col-4 sm:col-12">
+                                <a href={category.link} target="" rel="noopener noreferrer">
+                                    <div className="rounded-sm relative h-full shadow overflow-hidden">
+                                        <ImageFallback
+                                            src={category.imageSrc}
+                                            className="mx-auto pt-10"
+                                            width="90"
+                                            height="90"
+                                            alt={category.imageAlt}
+                                            priority
+                                        />
+                                        <div className="bg-body h-full rounded-b py-10 px-12">
+                                            <h3 className="text-dark mb-3">{category.title}</h3>
+                                            <p className="hidden sm:block">{category.description}</p>
+                                        </div>
+                                    </div>
+                                </a>
+                            </div>
+                        ))}
+                    </div>
+                </div>
+            </section>
+            <section className="section">
+                <div className="container">
+                    <div className="lg:col-5 mx-auto text-center">
+                    <h2>Frequently Asked Questions</h2>
+                    <p className="mt-4">
+                        Vestibulum ante ipsum primis in faucibus orci luctus ultrices
+                        posuere cubilia Curae Donec
+                    </p>
+                    </div>
+                    <div className="row justify-center mt-12">
+                    {mfFaq.map((faqItem, index) => (
+                        <Accordion key={index} title={faqItem.question}>
+                        {faqItem.answer}
+                        </Accordion>
+                    ))}
+                    </div>
+                </div>
+                </section>
         </main>
     );
 }
@@ -92,5 +190,5 @@ const imageUrls: string[] = [
     "/AMC/image_25.png",
     "/AMC/image_26.png",
     "/AMC/image_27.png",
-    "/AMC/image_28.png"
+    "/AMC/image_28.png",
 ];
--- src/components/common/Accordion.tsx
+++ src/components/common/Accordion.tsx
@@ -1,5 +1,4 @@
-"use client";
-
+"use client"
 import React, { useState } from "react";
 
 const Accordion = ({
@@ -13,23 +12,29 @@ const Accordion = ({
 }) => {
   const [show, setShow] = useState(false);
 
-  return (
-    <div className="col:lg-8">
-      <div className={`accordion ${show ? "active" : ""}`}>
-        <button className="accordion-header" onClick={() => setShow(!show)}>
-          <h5>{title}</h5>
+  const toggleAccordion = () => {
+    setShow((prev) => !prev);
+  };
 
+  return (
+    <div className={`col-lg-8 ${className}`}>
+      <div className={`accordion ${show ? "active" : ""}`}>
+        <button className="accordion-header" onClick={toggleAccordion}>
+          <h5>{title}</h5>
           <svg
             stroke="currentColor"
             fill="currentColor"
-            stroke-width="0"
+            strokeWidth="0"
             viewBox="0 0 448 512"
-            className="accordion-icon"
+            className={`accordion-icon ${show ? "open" : ""}`}
             height="1em"
             width="1em"
             xmlns="http://www.w3.org/2000/svg"
           >
-            <path d="M201.4 342.6c12.5 12.5 32.8 12.5 45.3 0l160-160c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L224 274.7 86.6 137.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3l160 160z"></path>
+            <path
+              fill="currentColor"
+              d="M201.4 342.6c12.5 12.5 32.8 12.5 45.3 0l160-160c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L224 274.7 86.6 137.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3l160 160z"
+            ></path>
           </svg>
         </button>
         {show && (
