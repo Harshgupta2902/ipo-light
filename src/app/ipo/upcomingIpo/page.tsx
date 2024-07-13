e3d2a1a30b02fab3e0f2c4e39c9fc322c38efeff
Harsh <harsh.gupta@cognustechnology.com>
2024-07-13 15:32:02 +0530

    loadiong addded in upcoming ipo

--- src/app/ipo/upcomingIpo/page.tsx
+++ src/app/ipo/upcomingIpo/page.tsx
@@ -1,28 +1,62 @@
 "use client";
 
 import axios from "axios";
-
 import { endpoints } from "@/api/endpoints";
 import UpcomingDataTable from "@/components/ipo/upcomingIpo/UpcomingDataTables";
 import UpcomingFaq from "@/components/ipo/upcomingIpo/upcomingFaq";
+import { useEffect, useState } from "react";
+import { get } from "@/api/api";
 
-const Home = async () => {
-  let result = null;
+// const Home = async () => {
+//   let result = null;
 
-  try {
-    const response = await axios.get(
-      endpoints.upcomingIpo,);
-    result = response.data;
-  } catch (error) {
-    console.error("Error fetching menu data:", error);
-  }
+//   try {
+//     const response = await axios.get(
+//       endpoints.upcomingIpo,);
+//     result = response.data;
+//   } catch (error) {
+//     console.error("Error fetching menu data:", error);
+//   }
+
+//   return (
+//     <>
+//       <UpcomingDataTable data={result} />
+//       <UpcomingFaq />
+//     </>
+//   );
+// };
+
+// export default Home;
+
+
+
+const Home: React.FC = () => {
+  const [result, setResult] = useState<any>(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await get(endpoints.upcomingIpo);
+        setResult(response);
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      } finally {
+        console.log('Api fetching data Done:');
+
+      }
+    };
+
+    fetchData();
+  }, [])
 
   return (
     <>
-      <UpcomingDataTable data={result} />
+      {result ? <UpcomingDataTable data={result} /> : <p>Loading...</p>}
       <UpcomingFaq />
     </>
   );
+
 };
+
 
 export default Home;
