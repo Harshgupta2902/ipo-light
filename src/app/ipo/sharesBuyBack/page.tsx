b10ee26ac44fcadeb7c5911907922d5130071b67
Harsh <harsh.gupta@cognustechnology.com>
2024-07-22 11:46:04 +0530

    buyback is now server side

--- src/app/ipo/sharesBuyBack/page.tsx
+++ src/app/ipo/sharesBuyBack/page.tsx
@@ -1,21 +1,28 @@
-"use client";
-
-import axios from "axios";
-
 import { endpoints } from "@/api/endpoints";
 import BuyBackDataTables from "@/components/ipo/sharesBuyBack/BuyBackDataTables";
 import BuyBackFaq from "@/components/ipo/sharesBuyBack/BuyBackFaq";
+
+
+const fetchBuybackIpo = async () => {
+  try {
+    const response = await fetch(endpoints.ipoBuyBack);
+    if (!response.ok) {
+      throw new Error("Data not found");
+    }
+    return await response.json();
+  } catch (error) {
+    console.error("Error fetching BuybackIpo", error);
+    throw error;
+  }
+};
 
 const Home = async () => {
   let result = null;
 
   try {
-    const response = await axios.get(
-      endpoints.ipoBuyBack,
-    );
-    result = response.data;
-  } catch (error) {
-    console.error("Error fetching menu data:", error);
+    result = await fetchBuybackIpo();
+  } catch (err) {
+    console.error(`error ${err}`);
   }
 
   return (
