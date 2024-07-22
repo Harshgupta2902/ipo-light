c7bcd167d8b82e64937bd28186c83c4937dc23b5
Harsh <harsh.gupta@cognustechnology.com>
2024-07-22 11:44:50 +0530

    sme is now server side

--- src/app/ipo/smeMarketIpo/page.tsx
+++ src/app/ipo/smeMarketIpo/page.tsx
@@ -1,22 +1,29 @@
-"use client";
-
-import axios from "axios";
-
-
 import { endpoints } from "@/api/endpoints";
 import SmeDataTable from "@/components/ipo/smeMarketIpo/SmeDataTables";
 import SmeFaq from "@/components/ipo/smeMarketIpo/SmeFaq";
 
+
+const fetchSmeIpo = async () => {
+  try {
+    const response = await fetch(endpoints.smeipo);
+    if (!response.ok) {
+      throw new Error("Data not found");
+    }
+    return await response.json();
+  } catch (error) {
+    console.error("Error fetching SmeIpo", error);
+    throw error;
+  }
+};
+
+
+
 const Home = async () => {
   let result = null;
-
   try {
-    const response = await axios.get(
-      endpoints.smeipo,
-    );
-    result = response.data;
-  } catch (error) {
-    console.error("Error fetching menu data:", error);
+    result = await fetchSmeIpo();
+  } catch (err) {
+    console.error(`error ${err}`);
   }
 
   return (
