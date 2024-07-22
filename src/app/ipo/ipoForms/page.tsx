a9d47af3ae02fac015e18187d19447fec01627f5
Harsh <harsh.gupta@cognustechnology.com>
2024-07-22 11:49:22 +0530

    forms is now server side

--- src/app/ipo/ipoForms/page.tsx
+++ src/app/ipo/ipoForms/page.tsx
@@ -1,21 +1,29 @@
-"use client";
-
-import axios from "axios";
-
 import { endpoints } from "@/api/endpoints";
 import FormsDataTable from "@/components/ipo/ipoForms/FormsDataTables";
 import FormsFaq from "@/components/ipo/ipoForms/FormsFaq";
+
+const fetchFormsIpo = async () => {
+  try {
+    const response = await fetch(endpoints.ipoForms);
+    if (!response.ok) {
+      throw new Error("Data not found");
+    }
+    return await response.json();
+  } catch (error) {
+    console.error("Error fetching FormsIpo", error);
+    throw error;
+  }
+};
+
+
 
 const Home = async () => {
   let result = null;
 
   try {
-    const response = await axios.get(
-      endpoints.ipoForms,
-    );
-    result = response.data;
-  } catch (error) {
-    console.error("Error fetching menu data:", error);
+    result = await fetchFormsIpo();
+  } catch (err) {
+    console.error(`error ${err}`);
   }
 
   return (
