de91d96b46a281ec33099e5d61cb62ec103f1498
Harsh <harsh.gupta@cognustechnology.com>
2024-07-22 11:51:18 +0530

    gmp is now server side

--- src/app/ipo/greyMarketIpo/page.tsx
+++ src/app/ipo/greyMarketIpo/page.tsx
@@ -1,22 +1,30 @@
-"use client";
-
-import axios from "axios";
-
 import { endpoints } from "@/api/endpoints";
 import GmpDataTables from "@/components/ipo/greyMarketIpo/GmpDataTables";
 import OldGmpDataTables from "@/components/ipo/greyMarketIpo/OldGmpDataTables";
 import GmpFaq from "@/components/ipo/greyMarketIpo/GmpFaq";
 
+
+const fetchGmpIpo = async () => {
+  try {
+    const response = await fetch(endpoints.gmpIpo);
+    if (!response.ok) {
+      throw new Error("Data not found");
+    }
+    return await response.json();
+  } catch (error) {
+    console.error("Error fetching GmpIpo", error);
+    throw error;
+  }
+};
+
+
 const Home = async () => {
   let result = null;
 
   try {
-    const response = await axios.get(
-      endpoints.gmpIpo,
-    );
-    result = response.data;
-  } catch (error) {
-    console.error("Error fetching menu data:", error);
+    result = await fetchGmpIpo();
+  } catch (err) {
+    console.error(`error ${err}`);
   }
 
   return (
