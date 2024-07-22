903075938605055688428e62d4001e38a6b8331c
Harsh <harsh.gupta@cognustechnology.com>
2024-07-22 11:42:29 +0530

    subscription is now server side

--- src/app/ipo/subscriptionStatus/page.tsx
+++ src/app/ipo/subscriptionStatus/page.tsx
@@ -6,16 +6,28 @@ import { endpoints } from "@/api/endpoints";
 import SubsCriptionDataTable from "@/components/ipo/subscriptionStatus/SubsCriptionDataTables";
 import SubscriptionFaq from "@/components/ipo/subscriptionStatus/SubsCriptionFaq";
 
+
+
+const fetchSubscriptionIpo = async () => {
+  try {
+    const response = await fetch(endpoints.iposubs);
+    if (!response.ok) {
+      throw new Error("Data not found");
+    }
+    return await response.json();
+  } catch (error) {
+    console.error("Error fetching SubscriptionIpo", error);
+    throw error;
+  }
+};
+
+
 const Home = async () => {
   let result = null;
-
   try {
-    const response = await axios.get(
-      endpoints.iposubs,
-    );
-    result = response.data;
-  } catch (error) {
-    console.error("Error fetching menu data:", error);
+    result = await fetchSubscriptionIpo();
+  } catch (err) {
+    console.error(`error ${err}`);
   }
 
   return (
