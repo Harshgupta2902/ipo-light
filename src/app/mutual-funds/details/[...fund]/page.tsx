b72c532cf15aedfa40f0d6641331957732b92c49
Harsh <harsh.gupta@cognustechnology.com>
2024-07-23 16:14:46 +0530

    mf details meta edited

--- src/app/mutual-funds/details/[...fund]/page.tsx
+++ src/app/mutual-funds/details/[...fund]/page.tsx
@@ -15,6 +15,60 @@ const fetchMfDetails = async (fundCode: any) => {
     }
 }
 
+
+export async function generateMetadata({ params }: { params: { fund: string[] } }) {
+    const pathname = params.fund.join("-");
+    const fundCode = pathname.split("-").pop();
+    let mfHomeData = null;
+    try {
+        mfHomeData = await fetchMfDetails(fundCode);
+    } catch (err) {
+        console.error(`Error fetching metadata: ${err}`);
+    }
+
+    const meta = mfHomeData.summary.meta;
+    const metaTitle = meta.fullName ? mfHomeData.summary.meta : pathname;
+    const metaDescription = `Explore the ${meta.fullName} (${meta.plan} Plan) with comprehensive details on performance, risk classification, benchmark index, and investment strategy. Invest wisely with ${meta.amc}.`;
+    const keywords = [
+        meta.fullName,
+        meta.sector,
+        meta.subsector,
+        meta.amc,
+        meta.benchmarkIndex,
+        "mutual fund investment",
+        "high risk mutual funds",
+        "debt and equity funds",
+    ];
+    return {
+        title: metaTitle,
+        description: metaDescription,
+        robots: "index, follow",
+        author: "IpoTech",
+        keywords: keywords,
+        copyright: "Copyright 2024 @ IpoTech",
+        url: "https://node.onlineinfotech.net",
+        openGraph: {
+            title: metaTitle,
+            description: metaDescription,
+            site: "https://node.onlineinfotech.net",
+            images: "https://node.onlineinfotech.net/logo.png",
+            type: "website",
+            url: `https://node.onlineinfotech.net${pathname}`,
+        },
+        twitter: {
+            card: "summary_large_image",
+            title: metaTitle,
+            description: metaDescription,
+            images: "https://www.gradding.com/logo.png"
+        },
+        alternates: {
+            canonical: `https://node.onlineinfotech.net${pathname}`,
+        },
+    };
+}
+
+
+
 const MutualFundsDetails = async ({ params, searchParams }: { params: { fund: string[] }, searchParams: { tab?: string } }) => {
     const fundName = params.fund.join("-");
     const fundCode = fundName.split("-").pop();
