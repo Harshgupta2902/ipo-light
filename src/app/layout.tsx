f5d2168b1a89194b804b271cffd74419d3ecaf4e
Harsh <harsh.gupta@cognustechnology.com>
2024-07-31 16:57:57 +0530

    adsense added

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -156,6 +156,11 @@ const RootLayout = async ({ children }: Readonly<{ children: React.ReactNode }>)
     <html lang="en">
       <head>
 
+        <script
+          async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3578725480736580"
+          crossOrigin="anonymous"
+        ></script>
         <meta name="google-site-verification" content="qfvtOvETSlOGGfLBvcE6Yk2Fqj0HmCGkmObv5r3MCnc" />
         <meta name="ahrefs-site-verification" content="c9bc1dfb881082e1aca65a8f84eb9243001c319904258b4f601717781f2339b3" />
         <meta name="msvalidate.01" content="E31009FF5E0FF8B37698B8D4B526016D" />
