19a96d949614a3ecb09f91ee4bc8181261b59068
Harsh <harsh.gupta@cognustechnology.com>
2024-08-03 11:01:32 +0530

    redirection added

--- src/middleware.js
+++ src/middleware.js
@@ -1,11 +1,45 @@
+// import { NextResponse } from "next/server";
+
+// export default async function middleware(request) {
+//   const url = request.nextUrl;
+//   const searchParams = url.searchParams;
+//   const pathname = url.pathname;
+//   const response = NextResponse.next();
+//   if (pathname.indexOf(".") === -1) {
+//     response.headers.append("x-url", pathname);
+//   }
+
+//   return response;
+// }
+
+// export const config = {
+//   runtime: "experimental-edge",
+// };
+
 import { NextResponse } from "next/server";
+
+const redirects = {
+  "/mutualfunds/details/tata-small-cap-fund-M_TASC":
+    "/mutualfunds/details/tata-small-cap-fund-direct-growth",
+  "/mutualfunds/details/tata-small-cap-fundidcw-payout-M_TASS":
+    "/mutualfunds/screener",
+  "/mutualfunds/details/nippon-india-gold-savings-fund-M_NIGU":
+    "/mutualfunds/screener",
+};
 
 export default async function middleware(request) {
   const url = request.nextUrl;
-  const searchParams = url.searchParams;
   const pathname = url.pathname;
+
+  if (redirects[pathname]) {
+    const url = request.nextUrl.clone();
+    url.pathname = redirects[pathname];
+    return NextResponse.redirect(url);
+  }
+
   const response = NextResponse.next();
-  if (pathname.indexOf(".") === -1) {
+
+  if (!pathname.includes(".")) {
     response.headers.append("x-url", pathname);
   }
 
@@ -13,5 +47,6 @@ export default async function middleware(request) {
 }
 
 export const config = {
+  matcher: "/mutualfunds/details/:path*",
   runtime: "experimental-edge",
 };
