1ba434544cdcea991c797521655b34e63dc3231d
Harsh <harsh.gupta@cognustechnology.com>
2024-08-05 10:44:05 +0530

    home page dynamic

--- src/components/ipo/home/HomeDataTables.tsx
+++ src/components/ipo/home/HomeDataTables.tsx
@@ -7,62 +7,58 @@ const UpcomingIpo: React.FC<IpoMainHomePageProps> = ({ upcomingData, smeData }) 
   const keystoshow = ["company_name", "open", "close"];
   const headers = ["Company Name", "Open", "Close"];
 
-  const filterData = (data: IpoData[]) =>
-    data.filter(item => item.company_name || item.open || item.close);
-
-  const filteredUpcomingData = filterData(upcomingData);
-  const filteredSmeData = filterData(smeData);
-
   return (
     <section>
       <div className="container">
         <div className="content">
           <div className="row justify-center text-left">
-            <div className="lg:col-6 md:col-6 mb-8 text-center">
-              <h2 className="mb-4">Upcoming Ipo's</h2>
-              <div className="relative overflow-x-auto">
-                <table className="lg:w-full text-sm text-left text-gray-500 ">
-                  {headers && headers.length > 0 && (
-                    <thead className="text-md text-gray-700 bg-gray-50 ">
-                      <tr>
-                        {headers.map((header, index) => (
-                          <th key={index} className="py-2">
-                            {header}
-                          </th>
-                        ))}
-                      </tr>
-                    </thead>
-                  )}
-                  <tbody>
-                    {filteredUpcomingData.map((item, index) => (
-                      <tr key={index}>
-                        {keystoshow.map((key) => (
-                          <td key={key} className="py-3">
-                            {key === "company_name" ? (
-                              <Link
-                                prefetch={false}
-                                style={{
-                                  textDecoration: "none",
-                                  fontSize: "medium",
-                                }}
-                                href={"/ipo/details/" + item.slug}
-                                target={"_blank"}
-                              >
-                                {item[key as keyof IpoData]}
-                              </Link>
-                            ) : (
-                              item[key as keyof IpoData]
-                            )}
-                          </td>
-                        ))}
-                      </tr>
-                    ))}
-                  </tbody>
-                </table>
+            {upcomingData && (
+              <div className="lg:col-6 md:col-6 mb-8 text-center">
+                <h2 className="mb-4">Upcoming Ipo's</h2>
+                <div className="relative overflow-x-auto">
+                  <table className="lg:w-full text-sm text-left text-gray-500 ">
+                    {headers && headers.length > 0 && (
+                      <thead className="text-md text-gray-700 bg-gray-50 ">
+                        <tr>
+                          {headers.map((header, index) => (
+                            <th key={index} className="py-2">
+                              {header}
+                            </th>
+                          ))}
+                        </tr>
+                      </thead>
+                    )}
+                    <tbody>
+                      {upcomingData.map((item, index) => (
+                        <tr key={index}>
+                          {keystoshow.map((key) => (
+                            <td key={key} className="py-3">
+                              {key === "company_name" ? (
+                                <Link
+                                  prefetch={false}
+                                  style={{
+                                    textDecoration: "none",
+                                    fontSize: "medium",
+                                  }}
+                                  href={"/ipo/details/" + item.slug}
+                                  target={"_blank"}
+                                >
+                                  {item[key as keyof IpoData]}
+                                </Link>
+                              ) : (
+                                item[key as keyof IpoData]
+                              )}
+                            </td>
+                          ))}
+                        </tr>
+                      ))}
+                    </tbody>
+                  </table>
+                </div>
               </div>
-            </div>
+            )}
 
-            <div className="lg:col-6 md:col-6 mb-8 text-center">
+            {smeData && (<div className="lg:col-6 md:col-6 mb-8 text-center">
               <h2 className="mb-4">SME Ipo's</h2>
               <div className="relative overflow-x-auto">
                 <table className="lg:w-full text-sm text-left text-gray-500">
@@ -78,11 +74,11 @@ const UpcomingIpo: React.FC<IpoMainHomePageProps> = ({ upcomingData, smeData }) 
                     </thead>
                   )}
                   <tbody>
-                    {filteredSmeData.map((item, index) => (
+                    {smeData.map((item, index) => (
                       <tr key={index}>
                         {keystoshow.map((key) => (
                           <td key={key} className="py-3">
-                           
+
                             {key === "company_name" ? (
                               <Link
                                 prefetch={false}
@@ -105,7 +101,7 @@ const UpcomingIpo: React.FC<IpoMainHomePageProps> = ({ upcomingData, smeData }) 
                   </tbody>
                 </table>
               </div>
-            </div>
+            </div>)}
           </div>
         </div>
       </div>
