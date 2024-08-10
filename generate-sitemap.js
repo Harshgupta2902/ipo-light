45d25203d6658bba11cfa411a6f97aa05e463380
Harsh <harsh.gupta@cognustechnology.com>
2024-08-10 15:55:43 +0530

    amc sitemap added

--- generate-sitemap.js
+++ generate-sitemap.js
@@ -4,12 +4,55 @@ const path = require("path");
 async function fetchLinks(url) {
   const response = await fetch(url);
   const data = await response.json();
-  // console.log(data);
   return data;
 }
 
 async function generateSitemap() {
   const currentDate = new Date().toISOString();
+  const amc = [
+    "Bajaj Finserv Mutual Fund",
+    "Helios Mutual Fund",
+    "WhiteOak Capital Mutual Fund",
+    "ITI Mutual Fund",
+    "TRUST Mutual Fund",
+    "NJ Mutual Fund",
+    "Samco Mutual Fund",
+    "Mahindra Manulife Mutual Fund",
+    "Canara Robeco Mutual Fund",
+    "Quant Mutual Fund",
+    "ICICI Prudential Mutual Fund",
+    "JM Financial Mutual Fund",
+    "LIC Mutual Fund",
+    "Franklin Templeton Mutual Fund",
+    "Aditya Birla Sun Life Mutual Fund",
+    "Mirae Asset Mutual Fund",
+    "Bank of India Mutual Fund",
+    "Motilal Oswal Mutual Fund",
+    "PGIM India Mutual Fund",
+    "360 ONE Mutual Fund",
+    "Nippon India Mutual Fund",
+    "Union Mutual Fund",
+    "Bandhan Mutual Fund",
+    "Navi Mutual Fund",
+    "SBI Mutual Fund",
+    "DSP Mutual Fund",
+    "Tata Mutual Fund",
+    "Edelweiss Mutual Fund",
+    "Invesco Mutual Fund",
+    "Sundaram Mutual Fund",
+    "HDFC Mutual Fund",
+    "HSBC Mutual Fund",
+    "PPFAS Mutual Fund",
+    "Baroda BNP Paribas Mutual Fund",
+    "Quantum Mutual Fund",
+    "Taurus Mutual Fund",
+    "Shriram Mutual Fund",
+    "Groww Mutual Fund",
+    "Kotak Mahindra Mutual Fund",
+    "Zerodha Mutual Fund",
+    "Axis Mutual Fund",
+    "UTI Mutual Fund",
+  ];
 
   const links1 = await fetchLinks(
     "https://apis-iota-five.vercel.app/api/getIpoLinks"
@@ -29,16 +72,33 @@ async function generateSitemap() {
       .join("")}
   </urlset>`;
 
-  const links2 = await fetchLinks(
-    "https://apis-iota-five.vercel.app/api/getMfLinks"
-  );
-  const sitemap2 = `<?xml version="1.0" encoding="UTF-8"?>
+  // const links2 = await fetchLinks(
+  //   "https://apis-iota-five.vercel.app/api/getMfLinks"
+  // );
+  // const sitemap2 = `<?xml version="1.0" encoding="UTF-8"?>
+  // <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+  //   ${links2
+  //     .map(
+  //       (link) => `
+  //     <url>
+  //       <loc>https://www.ipotec.in/mutualfunds/details/${link}</loc>
+  //       <lastmod>${currentDate}</lastmod>
+  //       <priority>0.80</priority>
+  //     </url>
+  //   `
+  //     )
+  //     .join("")}
+  // </urlset>`;
+
+  const sitemap3 = `<?xml version="1.0" encoding="UTF-8"?>
   <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-    ${links2
+    ${amc
       .map(
         (link) => `
       <url>
-        <loc>https://www.ipotec.in/mutualfunds/details/${link}</loc>
+        <loc>https://www.ipotec.in/mutualfunds/amc/${link
+          .toLowerCase()
+          .replaceAll(" ", "-")}</loc>
         <lastmod>${currentDate}</lastmod>
         <priority>0.80</priority>
       </url>
@@ -53,9 +113,10 @@ async function generateSitemap() {
   }
 
   fs.writeFileSync(path.join(publicDir, "ipo-details.xml"), sitemap1);
-  fs.writeFileSync(path.join(publicDir, "mutualfunds-details.xml"), sitemap2);
+  // fs.writeFileSync(path.join(publicDir, "mutualfunds-details.xml"), sitemap2);
+  fs.writeFileSync(path.join(publicDir, "amc.xml"), sitemap3);
   console.log(
-    "Sitemaps generated at public/ipo-details.xml and public/mutualfunds-details.xml"
+    "Sitemaps generated at public/ipo-details.xml and public/mutualfunds-details.xml and public/amc.xml "
   );
 }
 
