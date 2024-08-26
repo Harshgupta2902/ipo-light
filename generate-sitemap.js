6d8521d11fc848260e45f67cdaeb54feae7a7c50
Harsh <harsh.gupta@cognustechnology.com>
2024-08-26 10:25:20 +0530

    blogs sitemap changed

--- generate-sitemap.js
+++ generate-sitemap.js
@@ -4,8 +4,6 @@ const path = require("path");
 async function fetchLinks(url) {
   const response = await fetch(url);
   const data = await response.json();
-  console.log(data);
-  
   return data;
 }
 
